905a4d8fe653487e5e5b57347ecf2b1fd2a4876c
johnmorgan25 <johnemadmorgan@gmail.com>
2021-09-13 23:27:56 +0200

    Create Tutorial.js

---
+++ udemy-modernJavaScriptBootcamp/1. JavaScript- Variables and Flow Control/Tutorial.js
@@ -0,0 +1,7 @@
+// This tutorial teaches: Basics of varibles , Concatination
+
+let firstName = "Andrew";
+let lastName = "Mead";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
