2b15da56e5ae5b50501601683c06cacec4a9a7b7
JOHN MORGAN <johnemadmorgan@gmail.com>
2021-10-12 17:00:24 +0200

    12/10

---
+++ Eloquent Javascript/3- Functions/3.nestedScope.js
@@ -0,0 +1,12 @@
+// in JS blocks and functions can be created inside blocks and functions 
+// this produces multible degrees of locality 
+
+const hummus = function(factor){
+    const ingredient = function(amount, unit, namee){
+        let ingredientAmount = amount*factor;
+        if (ingredientAmount > 1){
+            unit += "s";
+        }
+        console.log(`${ingredientAmount} ${unit}`,${namee})
+    }
+}
---
+++ udemy-modernJavaScriptBootcamp/4. JavaScript - Arrays/playGround.js
@@ -0,0 +1,16 @@
+// This is a playground for javascript arrays 
+
+//concat 
+let testArray = ['a','b','c',4,5,6];
+testArray = testArray.concat(['x']);
+console.log(testArray);
+// Output:
+// ['a','b','c',4,5,6,'x']
+
+
+//join
+testArray = ['a','b','c',4,5,6];
+testArray = testArray.join('-');
+console.log(testArray);
+// Output:
+// a-b-c-4-5-6
