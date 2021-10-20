a3ab5dcbc1c4862546bc5b78d1f184ea32dbe031
JOHN MORGAN <johnemadmorgan@gmail.com>
2021-10-20 08:23:04 +0200

    10/10

---
+++ Eloquent Javascript/3- Functions/arrowFunction.js
@@ -0,0 +1,12 @@
+// There are three ways to declare a function 
+// First way 
+const square1 = function(x){
+    return x*x;
+};
+// This method requires a semi colon 
+
+// second way 
+function square2 (x){
+    return x*x;
+}
+// This m
